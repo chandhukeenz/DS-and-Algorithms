4750d49110d5e0d28ee9b05ac4d89c47231a563e
chandhukeenz <55447161+chandhukeenz@users.noreply.github.com>
2020-09-28 23:26:34 +0530

    Add files via upload

---
+++ leetcode/array and map/product of array except self.cpp
@@ -0,0 +1,28 @@
+//product of array except self
+//Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i]
+//Input:  [1,2,3,4]
+//Output: [24,12,8,6]
+//Constraint: It's guaranteed that the product of the elements of any prefix or suffix of the array (including the whole array) fits in a 32 bit integer.
+
+//Note: Please solve it without division and in O(n).
+
+//Follow up:
+//Could you solve it with constant space complexity? (The output array does not count as extra space for the purpose of space complexity analysis.)
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int size = nums.size();
+        vector<int> output(size);
+        int x=1;
+        output[0]=1;
+        for(int i = 1 ; i < size ; i++){
+            output[i] = nums[i-1]*output[i-1];
+        }
+        for(int i = size-1 ; i >= 0 ; i--){
+            output[i] = output[i]*x;
+            x = x*nums[i];
+        }
+        return output;
+    }
+};
