6d18ef847c3fa1cad5a8a8901b571c9b5f5adc60
chandhukeenz <55447161+chandhukeenz@users.noreply.github.com>
2020-12-30 11:09:52 +0530

    Add files via upload

---
+++ 100daysofcode/set matrix zeroes.cpp
@@ -0,0 +1,34 @@
+//Set Matrix Zeroes
+//Given an m x n matrix. If an element is 0, set its entire row and column to 0. Do it in-place.
+
+//Follow up:
+
+//A straight forward solution using O(mn) space is probably a bad idea.
+//A simple improvement uses O(m + n) space, but still not the best solution.
+//Could you devise a constant space solution?
+
+//Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+//Output: [[1,0,1],[0,0,0],[1,0,1]]
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int r=matrix.size(),c=matrix[0].size(),col=1;
+        
+        for(int i=0;i<r;i++){
+            if(matrix[i][0]==0) col=0;
+            for(int j=1;j<c;j++)
+                if(matrix[i][j]==0) matrix[i][0]=matrix[0][j]=0;
+        }
+        for(int i=r-1;i>=0;i--){
+            for(int j=c-1;j>=1;j--)
+                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+            if(col==0) matrix[i][0]=0;
+        }
+    }
+};
+
+//use the first row and column as an entry for the presence of zero in its' row or colomn.
+//have col as 0 if there is any zero in the first column.[j=0 is checked separately and its' entry is done in col](j starts from 1)
+//now backtrace the 2d array and convert each number to zero by checking the entries in the flag row and column
+//(again j=0 is separately traversed while checking col)
