64fc7765d339369f8b33700e4a2a0094a089d453
chandhana <chan.keen3112000@gmail.com>
2021-01-08 20:09:12 +0530

    add jan 8

---
+++ 100daysofcode/indian_coin_change_greedy.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int make_change(int *coins,int n,int money){
+    int ans=0;
+    while(money>0){
+        int idx = upper_bound(coins,coins+n,money)-1-coins;
+        money =money - coins[idx];
+        ans++;
+    }
+    return ans;
+}
+
+int main(){
+    int money;
+    cin>>money;
+    int coins[]={1,2,5,10,100,200,500,2000};
+    int t=sizeof(coins)/sizeof(int);
+    cout<<"coins needed "<<make_change(coins,t,money);
+    return 0;
+}
