9f51d60bb885dab106d83afbafe27178164820e8
chandhana <chan.keen3112000@gmail.com>
2021-02-15 23:11:03 +0530

    add feb15

--- 100daysofcode/bst.cpp
+++ 100daysofcode/bst.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<queue>
+#include<unordered_map>
 using namespace std;
 
 struct Node{
@@ -7,30 +9,15 @@ struct Node{
     struct Node *right;
 };
 
-void preorder(struct Node *root){
-    if(root==NULL) return;
-    cout<<root->data<<" ";
-    preorder(root->left);
-    preorder(root->right);
-}
-void inorder(struct Node *root){
-    if(root==NULL) return;
-    inorder(root->left);
-    cout<<root->data<<" ";
-    inorder(root->right);
-}
-void postorder(struct Node *root){
-    if(root==NULL) return;
-    postorder(root->left);
-    postorder(root->right);
-    cout<<root->data<<" ";
-}
+//create new node
 struct Node* getnewnode(int data){
     Node* newnode =new Node();
     newnode->data=data;
     newnode->left=newnode->right=NULL;
     return newnode;
 }
+
+//insert the newnode into the bst tree
 struct Node* insert(struct Node* root,int data){
     if(root==NULL){
         root=getnewnode(data);
@@ -43,6 +30,7 @@ struct Node* insert(struct Node* root,int data){
     return root;
 }
 
+//search the element in the tree
 bool search(struct Node* root,int data){
     if(root==NULL) return false;
     else if(root->data==data) return true;
@@ -50,18 +38,84 @@ bool search(struct Node* root,int data){
     else return search(root->right,data);
 }
 
+//ROOT - LEFT - RIGHT
+void preorder(struct Node *root){
+    if(root==NULL) return;
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+//LEFT - ROOT - RIGHT
+void inorder(struct Node *root){
+    if(root==NULL) return;
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+//LEFT - RIGHT - ROOT
+void postorder(struct Node *root){
+    if(root==NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+//Print the tree in the level order in a single line
+void levelorderInaLine(struct Node *root,queue<struct Node*>q){
+    if(q.empty() || root==NULL) return;
+    cout<<root->data<<" ";
+    if(root->left)q.push(root->left);
+    if(root->right)q.push(root->right);
+    q.pop();
+    levelorderInaLine(q.front(),q);
+}
+
+//Print the tree in the level order in level by level format
+void levelorderLbyL(struct Node *root,queue<struct Node*>q,int c){
+    if(q.empty()||c==2) return;
+    if(root==NULL){
+        c++;
+        cout<<'\n';
+        q.push(NULL);
+    }else{
+        c=0;
+        if(root->left)q.push(root->left);
+        if(root->right)q.push(root->right);
+        cout<<root->data<<" ";
+    }
+    q.pop();
+    levelorderLbyL(q.front(),q,c);
+}
+
 int main(){
     struct Node *root=NULL;
+    queue<struct Node*>q1,q2;
+    queue<pair<struct Node *,int>>q3;
+    int countNull=0;
     root= insert(root,9); root= insert(root,1);
     root= insert(root,6);root= insert(root,78);
     root= insert(root,11);root= insert(root,3);
-    cout<<"postorder:"<<endl;
-    postorder(root);
-    cout<<"\ninorder:"<<endl;
-    inorder(root);
     cout<<"\npreorder:"<<endl;
     preorder(root);
-    cout<<endl;
+    cout<<"\ninorder:"<<endl;
+    inorder(root);
+    cout<<"postorder:"<<endl;
+    postorder(root);
+    cout<<"\nlevelorder in a single Line:"<<endl;
+    q1.push(root);
+    levelorderInaLine(root,q1);
+    cout<<"\nlevelorder printed level by level:"<<endl;
+    q2.push(root);q2.push(NULL);
+    levelorderLbyL(root,q2,countNull);
+    /*cout<<"\nVertical order:"<<endl;
+    q3.push(make_pair(root,0));
+    int vLevel=0;
+    unordered_map<int,vector<int>>m;
+    m[vLevel].push_back(root->data);
+    verticalorder(root,q3,vLevel,m);
+    */
     int number;
     cout<<"Enter the number to be searched:";
     cin>>number;
