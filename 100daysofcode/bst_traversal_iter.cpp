7e36e3acab9706a76a9aa4a7e01c50b1dac95c01
chandhana <chan.keen3112000@gmail.com>
2021-02-23 22:15:57 +0530

    add feb23

--- 100daysofcode/bst_traversal_iter.cpp
+++ 100daysofcode/bst_traversal_iter.cpp
@@ -85,9 +85,41 @@ void preorder(struct Node*root,stack<struct Node*>s){
         root=root->right;
     }
 }
+struct Node* findmin(struct Node* root){
+    while(root->left!=NULL) root=root->left;
+    return root;
+}
+
+struct Node* deleteNode(struct Node* root,int val){
+    if(root==NULL)return root;
+    else if(val<root->data) root->left=deleteNode(root->left,val);
+    else if(val>root->data) root->right=deleteNode(root->right,val);
+    else{
+        //no child
+        if(root->left==NULL && root->right==NULL){
+             delete root;
+             root=NULL;
+        }else if(root->left==NULL){
+            struct Node *temp=root;
+            root=root->right;
+            delete temp;
+        }else if(root->right==NULL){
+            struct Node *temp=root;
+            root=root->left;
+            delete temp;
+        }else{
+            struct Node* temp=findmin(root->right);
+            root->data=temp->data;
+            root->right=deleteNode(root->right,temp->data);
+        }
+    }
+    return root;
+}
+
 int main(){
     struct Node *root=NULL;
     stack<struct Node*>s1;
+    int n;
     root= insert(root,9); root= insert(root,1);
     root= insert(root,6);root= insert(root,78);
     root= insert(root,11);root= insert(root,3);
@@ -97,5 +129,10 @@ int main(){
     inorder(root,s1);
     cout<<"\npreorder:"<<endl;
     preorder(root,s1);
+    cout<<"\nenter the value to be deleted:"<<endl;
+    cin>>n;
+    root=deleteNode(root,n);
+    cout<<"After deleting..inorder traversal:"<<endl;
+    inorder(root,s1);
     return 0;
 }
