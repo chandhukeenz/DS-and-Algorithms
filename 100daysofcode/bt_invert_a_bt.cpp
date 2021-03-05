9a60bd0bb90c75178638e40ecc8ecbabc766427d
chandhana <chan.keen3112000@gmail.com>
2021-03-05 21:29:37 +0530

    add mar5

---
+++ 100daysofcode/bt_invert_a_bt.cpp
@@ -0,0 +1,49 @@
+/*
+Invert Binary Tree
+Invert a binary tree.
+
+Example:
+
+Input:
+
+     4
+   /   \
+  2     7
+ / \   / \
+1   3 6   9
+Output:
+
+     4
+   /   \
+  7     2
+ / \   / \
+9   6 3   1
+*/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        stack<TreeNode*>s;
+        s.push(root);
+        while (!s.empty()) {
+            TreeNode* p = s.top();
+            s.pop();
+            if (p){
+                s.push(p->left);
+                s.push(p->right);
+                swap(p->left, p->right);
+            }
+        }
+        return root;
+    }
+};
