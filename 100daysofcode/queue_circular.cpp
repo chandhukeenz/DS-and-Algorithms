840c97aa070485dd92cc4b97a2d31ee4d0b1b911
chandhana <chan.keen3112000@gmail.com>
2021-03-08 21:13:21 +0530

    add mar8

--- 100daysofcode/queue_circular.cpp
+++ 100daysofcode/queue_circular.cpp
@@ -10,11 +10,13 @@ struct circularQueue{
         a= new int[l];
     }
     void enqueue(int val){
-        if((front==0 && rear==length-1)||(rear==(front-1)%(length-1)))
+        if(front==(rear+1)%length){
                 cout<<"circular queue is full !!"<<endl;
+                return;
+        }
         else if(front==-1) 
                 front=rear=0;
-        else if(rear==length-1 && front!=0) 
+        else if(rear==(length-1) && front!=0) 
                 rear=0;
         else rear++;
 
@@ -23,9 +25,50 @@ struct circularQueue{
     }
 
     int dequeue(){
-        if(front==-1)
+        if(front==-1){
            cout<<"circular queue is empty!!"<<endl;
-        
+           return INT32_MIN;
+        }
+        int data = a[front];
+        a[front]=-1;
+        if(front==rear)
+            front=rear=-1;
+        else if(front==(length-1))
+            front=0;
+        else front++;
+        return data;
     }
-    void displayqueue();
+    void displayqueue(){
+        if(front==-1){
+            cout<<"\nQueue is empty";
+            return;
+        }
+        if(rear>=front){
+            for(int i=front;i<=rear;i++)
+                cout<<a[i]<<" ";
+        }else{
+            for(int i=front;i<length;i++)
+                cout<<a[i]<<" ";
+            for(int i=0;i<=rear;i++)
+                cout<<a[i]<<" ";
+        }
+    }
 };
+
+int main(){
+    struct circularQueue c(5);
+    c.enqueue(1);c.enqueue(-1);
+    c.enqueue(2);c.enqueue(-2);
+    c.enqueue(3);c.enqueue(-3);
+
+    c.displayqueue();
+    cout<<endl;
+    cout<<c.dequeue()<<endl;
+    cout<<c.dequeue()<<endl;
+    cout<<c.dequeue()<<endl;
+    cout<<c.dequeue()<<endl;
+    cout<<c.dequeue()<<endl;
+    cout<<c.dequeue()<<endl;
+    c.displayqueue();
+    return 0;
+}
