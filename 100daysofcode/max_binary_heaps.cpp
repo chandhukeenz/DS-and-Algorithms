13c4727075857ec97e9150b32bebb69fc4ed84af
chandhana <chan.keen3112000@gmail.com>
2021-03-13 22:26:28 +0530

    add mar13

--- 100daysofcode/max_binary_heaps.cpp
+++ 100daysofcode/max_binary_heaps.cpp
@@ -21,6 +21,47 @@ struct maxbinaryheap{
             idx=parentidx;
         }
     }
+    int extractMax(){
+		int max =values[0];
+		int end =values.back();
+        values.pop_back();
+		if(values.size()>0){
+			values[0]=end;
+		    sinkDown();	
+		}
+		return max;
+	}
+	void sinkDown(){
+		int idx=0;
+		int length =values.size();
+		int element = values[0];
+		while(true){
+			int leftChildIdx =2*idx+1;
+			int rightChildIdx =2*idx+2;
+			int leftChild,rightChild;
+			bool swap=false;
+            int s;
+			if(leftChildIdx<length){
+				leftChild=values[leftChildIdx];
+				if(leftChild>element){
+					s=leftChildIdx;
+                    swap=true;
+				}
+			}
+			if(rightChildIdx<length){
+				rightChild=values[rightChildIdx];
+				if((swap==false && rightChild>element)||
+				(swap!=false &&rightChild>leftChild)){
+					s=rightChildIdx;
+                    swap=true;
+				}
+			}
+			if(swap==false) break;
+			values[idx]=values[s];
+			values[s]=element;
+			idx=s;
+		}
+	}
     void display(){
         cout<<"The max binary heap:\n";
         for(int i=0;i<values.size();i++){
@@ -35,6 +76,16 @@ int main(){
     h.insert(8);h.insert(3);
     h.insert(15);
     h.insert(100);
+    h.display();
+    cout<<"\nmax val:"<<h.extractMax()<<endl;
+    h.display();
+    cout<<"\nmax val:"<<h.extractMax()<<endl;
+    h.display();
+    cout<<"\nmax val:"<<h.extractMax()<<endl;
+    h.display();
+    cout<<"\nmax val:"<<h.extractMax()<<endl;
+    h.display();
+    cout<<"\nmax val:"<<h.extractMax()<<endl;
     h.display();
     return 0;
 }
